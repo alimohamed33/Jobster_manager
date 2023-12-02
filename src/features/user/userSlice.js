1b242dcf84afcae147a3df5935f44a263e1bef48
“alimohamed33” <alimohamedali2110@gmail.com>
2023-12-03 00:04:33 +0200

    finish user slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -37,6 +37,26 @@ export const loginUser = createAsyncThunk(
   }
 );
 
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (user, thunkAPI) => {
+    try {
+      const response = await customFetch.patch("/auth/updateUser", user, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized! Logging out...");
+      }
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -66,6 +86,7 @@ const userSlice = createSlice({
       state.isLoading = false;
       toast.error(payload);
     },
+
     // Login
     [loginUser.pending]: (state) => {
       state.isLoading = true;
@@ -78,6 +99,22 @@ const userSlice = createSlice({
       toast.success(`Welcome Back ${user.name}`);
     },
     [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+
+    // update
+    [updateUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      addUserToLocalStorage(user);
+      toast.success(`User Updated`);
+    },
+    [updateUser.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
--- src/pages/Dashboard/Profile.jsx
+++ src/pages/Dashboard/Profile.jsx
@@ -1,4 +1,80 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { toast } from "react-toastify";
+import { FormRow } from "../../components";
+import { updateUser } from "../../features/user/userSlice";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const dispatch = useDispatch();
+  const { isLoading, user } = useSelector((store) => store.user);
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  });
+  const { name, email, lastName, location } = userData;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !lastName || !location) {
+      toast.error("please fill out all fields");
+      return;
+    }
+
+    dispatch(updateUser({ name, email, lastName, location }));
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit}>
+        <h3>profile</h3>
+
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={name}
+            handleChange={handleChange}
+          />
+
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={lastName}
+            handleChange={handleChange}
+          />
+
+          <FormRow
+            type="email"
+            name="email"
+            value={email}
+            handleChange={handleChange}
+          />
+
+          <FormRow
+            type="text"
+            name="location"
+            value={location}
+            handleChange={handleChange}
+          />
+
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? "please wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default Profile;
